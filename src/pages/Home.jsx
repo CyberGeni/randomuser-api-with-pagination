81c84a1482e9012b2bfc71515f809ec8825c9039
Treasure Ajefu <treasureajefu@gmail.com>
2022-10-31 22:26:55 +0000

    fix: adjusted lineheight of hero test and spacing of cta

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,12 @@ import Navbar from "../components/Navbar";
 
 function Home() {
     return ( 
-        <div className="font-['Hind_Madurai'] bg-slate-900 h-screen text-gray-200 px-10 md:px-16 py-6">
+        <div className="font-['Hind_Madurai'] bg-slate-900 h-screen text-gray-200 px-10 md:px-24 py-6">
             <Navbar />
             <main className="h-[75vh] flex flex-col items-center justify-center md:items-start">
-                <h1 className="text-5xl md:text-6xl leading-tight font-semibold my-6 md:max-w-4xl">You mean you just want users? Like, just actual users?</h1>
+                <h1 className="text-5xl md:text-6xl font-semibold my-6 md:max-w-4xl">You mean you just want users? Like, just actual users?</h1>
                 <p className="text-xl md:text-2xl my-5 max-w-3xl">We're widely known for the amount of users we have currently using our software. You can have access to all of them... for free</p>
-                <Link to="/users" className="transition-all border-emerald-600 border-2 text-emerald-600 uppercase px-5 py-3 my-2 rounded hover:outline-dashed hover:outline-offset-4">view our users</Link>
+                <Link to="/users" className="transition-all border-emerald-600 border-2 text-emerald-600 uppercase px-5 py-3 my-5 rounded hover:outline-dashed hover:outline-offset-4">view our users</Link>
             </main>
         </div>
      );
